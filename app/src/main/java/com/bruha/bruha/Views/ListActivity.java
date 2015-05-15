215930a71d3de11d80da9cba7981e32a0813e828
GithubBell <ishtiaba@mcmaster.ca>
2015-05-15 12:51:04 -0400

    Compiled the swipe list with gradle(added dependencies and repositories) Added text to styles and drawable to help implement the swipes Added Swipe List view to the xml file in activity_list.xml added code to implement swipe list in ListActivity Everything runs fine in code but it does not seem to swipe on the phone

--- app/src/main/java/com/bruha/bruha/Views/ListActivity.java
+++ app/src/main/java/com/bruha/bruha/Views/ListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,12 +23,16 @@ import android.widget.Toast;
 import com.bruha.bruha.Adapters.ListviewAdapter;
 import com.bruha.bruha.Model.Event;
 import com.bruha.bruha.R;
+import com.fortysevendeg.swipelistview.BaseSwipeListViewListener;
+import com.fortysevendeg.swipelistview.SwipeListView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
+
 public class ListActivity extends ActionBarActivity {
+
 
     public ListActivity()
     {}
@@ -37,6 +43,7 @@ public class ListActivity extends ActionBarActivity {
     @InjectView(R.id.MapButton) Button mMapButton;
     @InjectView(R.id.DashboardButton) Button mDashboardButton;
     @InjectView(android.R.id.list) ListView mListView;
+    @InjectView(R.id.example_swipe_lv_list) SwipeListView swipe;
 
 
 
@@ -66,9 +73,67 @@ public class ListActivity extends ActionBarActivity {
 
         ListviewAdapter adapter=new ListviewAdapter(this,mEvents); //Calling the adapter ListView to help set the List
 
+
+
+        swipe.setSwipeListViewListener(new BaseSwipeListViewListener() {
+            @Override
+            public void onOpened(int position, boolean toRight) {
+            }
+
+            @Override
+            public void onClosed(int position, boolean fromRight) {
+            }
+
+            @Override
+            public void onListChanged() {
+            }
+
+            @Override
+            public void onMove(int position, float x) {
+            }
+
+            @Override
+            public void onStartOpen(int position, int action, boolean right) {
+                Log.d("swipe", String.format("onStartOpen %d - action %d", position, action));
+            }
+
+            @Override
+            public void onStartClose(int position, boolean right) {
+                Log.d("swipe", String.format("onStartClose %d", position));
+            }
+
+            @Override
+            public void onClickFrontView(int position) {
+                Log.d("swipe", String.format("onClickFrontView %d", position));
+
+                swipe.openAnimate(position); //when you touch front view it will open
+
+            }
+
+            @Override
+            public void onClickBackView(int position) {
+                Log.d("swipe", String.format("onClickBackView %d", position));
+
+                swipe.closeAnimate(position);//when you touch back view it will close
+            }
+
+            @Override
+            public void onDismiss(int[] reverseSortedPositions) {
+
+            }
+
+        });
+
+        swipe.setSwipeMode(SwipeListView.SWIPE_MODE_LEFT); // there are five swiping modes
+        swipe.setSwipeActionLeft(SwipeListView.SWIPE_ACTION_REVEAL); //there are four swipe actions
+        swipe.setSwipeActionRight(SwipeListView.SWIPE_ACTION_REVEAL);
+        swipe.setOffsetLeft(convertDpToPixel(260f)); // left side offset
+        swipe.setOffsetRight(convertDpToPixel(0f)); // right side offset
+        swipe.setAnimationTime(50); // animarion time
+        swipe.setSwipeOpenOnLongPress(true); // enable or disable SwipeOpenOnLongPress
+
+
         mListView.setAdapter(adapter);
-
-
 
         //Setting an OnClickListener everytime a item of the list is tapped.
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -76,11 +141,11 @@ public class ListActivity extends ActionBarActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 
-               setPanelHeight();
+                setPanelHeight();
                 String name = mEvents[position].getEventName();
                 String date = mEvents[position].getEventDate();
                 String price = mEvents[position].getEventPrice() + "";
-                String distance= mEvents[position].getEventDistance()+ "";
+                String distance = mEvents[position].getEventDistance() + "";
                 String message = String.format("%s will be playing on %s and it will be %s and is at a distance of %s from you",
                         name,
                         date,
@@ -112,7 +177,16 @@ public class ListActivity extends ActionBarActivity {
 
 
 
+    public int convertDpToPixel(float dp) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return (int) px;
+    }
+
+
     public void setPanelHeight(){
+
+
 
 
 
@@ -136,6 +210,8 @@ public class ListActivity extends ActionBarActivity {
            int height = size.y;
 
 
+
+
         // Retrieves the current parameters of the layout and storing them in variable params
 
         ViewGroup.LayoutParams params = mLayouttoChange.getLayoutParams();
@@ -148,4 +224,8 @@ public class ListActivity extends ActionBarActivity {
         Log.v("height test", params.height + "");
 
     }
+
+
+
+
 }
