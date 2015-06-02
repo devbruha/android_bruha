6769ed0577646ee8dba62e0eaa0868864fc3be3e
Thomas Miele <thomas.miele@hotmail.com>
2015-06-02 09:52:03 -0400

    Added userLocation on the maps Activity

--- app/src/main/java/com/bruha/bruha/Views/MapsActivity.java
+++ app/src/main/java/com/bruha/bruha/Views/MapsActivity.java
@@ -1,13 +1,18 @@
 package com.bruha.bruha.Views;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bruha.bruha.Model.UserCustomFilters;
 import com.bruha.bruha.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -16,11 +21,28 @@ import com.google.android.gms.maps.model.LatLng;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private UserCustomFilters mUserCustomFilters = new UserCustomFilters();
+
+    protected static final String TAG = "basic-location-sample";
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+
+    protected String mLatitudeText;
+    protected String mLongitudeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +51,8 @@ public class MapsActivity extends FragmentActivity {
         ButterKnife.inject(this);
 
         // Map and Filter setup
+
+        buildGoogleApiClient();
 
         setUpMapIfNeeded();
         setUpFilters();
@@ -59,7 +83,32 @@ public class MapsActivity extends FragmentActivity {
 
         // Setting default location to mcmaster, eventually will be set to user location
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.2500,-79.919501), 14.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.2500, -79.919501), 14.0f));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     private void setUpFilters(){
@@ -92,7 +141,39 @@ public class MapsActivity extends FragmentActivity {
         Log.v("Big Filter Test", mUserCustomFilters.getQuickieFilter()+"");
         Log.v("Big Filter Test", mUserCustomFilters.getDateFilter()+"");
         Log.v("Big Filter Test", mUserCustomFilters.getCategoryFilter().keySet()+"");
-        Log.v("Big Filter Test", mUserCustomFilters.getAdmissionPriceFilter()+"");
+        Log.v("Big Filter Test", mUserCustomFilters.getAdmissionPriceFilter() + "");
 
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText =String.valueOf(mLastLocation.getLatitude());
+            mLongitudeText = String.valueOf(mLastLocation.getLongitude());
+
+            Log.v("location test", mLatitudeText + "    " + mLongitudeText);
+        } else {
+            Toast.makeText(this,"No location detected", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
