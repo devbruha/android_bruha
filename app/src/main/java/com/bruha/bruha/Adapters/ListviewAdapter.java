50abdd4768032070b4e548a5141c01aa008d212b
GithubBell <ishtiaba@mcmaster.ca>
2015-06-04 15:57:41 -0400

    list_item updated, added padding to all the description layout, removed using testround SO IT CAN NOW BE DELETED FROM APP,DELETE IT. Resizing of Everything in the ListView is implemented, the right swipe is resized,the text inside the bubble is resized and all the text inside the description layout is resized.

--- app/src/main/java/com/bruha/bruha/Adapters/ListviewAdapter.java
+++ app/src/main/java/com/bruha/bruha/Adapters/ListviewAdapter.java
@@ -2,6 +2,7 @@ package com.bruha.bruha.Adapters;
 
 import android.app.Activity;
 import android.graphics.Point;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +33,54 @@ public class ListviewAdapter extends BaseSwipeAdapter {
     public ListviewAdapter(Activity activity, ArrayList<Event> events) {
         mActivity = activity;
         mEvents = events;
+    }
+
+    //Method to Format the Date that will be displayed.
+    public String dateFormat(String Date)
+    {
+        String year=Date.substring(0,4);
+        String Month=Date.substring(5,7);
+        String Dates=Date.substring(8,10);
+        String Displayed=getMonth(Month)+ " " + Dates + "," + year;
+        return Displayed;
+    }
+
+    //Part of the Implementation of the dateFormat Method.
+    public String getMonth(String Month)
+    {
+        if(Month.equals("01"))
+        Month="January";
+        if(Month.equals("02"))
+            Month="Febuary";
+        if(Month.equals("03"))
+            Month="March";
+        if(Month.equals("04"))
+            Month="April";
+        if(Month.equals("05"))
+            Month="May";
+        if(Month.equals("06"))
+            Month="June";
+        if(Month.equals("07"))
+            Month="July";
+        if(Month.equals("08"))
+            Month="August";
+        if(Month.equals("09"))
+            Month="September";
+        if(Month.equals("10"))
+            Month="October";
+        if(Month.equals("11"))
+            Month="November";
+        if(Month.equals("12"))
+            Month="December";
+        return Month;
+    }
+
+    //Checks if it is a free event, if so, Displays it.
+    public String freeEventCheck(double price)
+    {
+        if(price==0.0)
+        {return "Free Event!";}
+        else {return "$"+price; }
     }
 
     @Override
@@ -136,8 +185,8 @@ public class ListviewAdapter extends BaseSwipeAdapter {
         //Setting the summary description
         holder.EventDistance.setText(event.getEventDistance()+"km");
         holder.EventName.setText(event.getEventName());
-        holder.EventDate.setText(event.getEventDate());
-        holder.EventPrice.setText("$"+event.getEventPrice());
+        holder.EventDate.setText(dateFormat(event.getEventDate()));
+        holder.EventPrice.setText(freeEventCheck(event.getEventPrice()));
         //holder.EventIcon.setImageResource(event.getEventIcon());
         //holder.EventPicture.setImageResource(event.getEventPicture());
 
@@ -147,9 +196,9 @@ public class ListviewAdapter extends BaseSwipeAdapter {
         holder.EventLocName.setText(event.getEventLocName());
         holder.EventLocSt.setText(event.getEventLocSt());
         holder.EventLocAdd.setText(event.getEventLocAdd());
-        holder.EventStartDate.setText(event.getEventDate());
+        holder.EventStartDate.setText(dateFormat(event.getEventDate()));
         holder.EventStartTime.setText(event.getEventStartTime());
-        holder.EventEndDate.setText(event.getEventEndDate());
+        holder.EventEndDate.setText(dateFormat(event.getEventEndDate()));
         holder.EventEndTime.setText(event.getEventEndTime());
 
         //Swipe methods being Implemented
@@ -183,26 +232,127 @@ public class ListviewAdapter extends BaseSwipeAdapter {
 
     @Override
     public void fillValues(int position, View convertView) {
-
+        //Assigning the ImageBubble to a variable to alter iits dimensions after with.
         ImageView circle = (ImageView) convertView.findViewById(R.id.EventImageBubble);
 
-        // Android functions to determine the screen dimensions
-
+        // Android functions to determine the screen dimensions.
         Display display = mActivity.getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
 
-        // Storing the screen height into an int variable
+        // Storing the screen height into an int variable.
         int height = size.y;
 
+        //Gets the Layout Params of the itemList.
         ViewGroup.LayoutParams params = convertView.getLayoutParams();
 
+        //Sets the height to 1/3 the screensize.
         params.height =  (int)Math.round(height*.33);
 
+        //Getting the LayoutParams of the circle and then setting it to quarter the screensize.
         ViewGroup.LayoutParams circleParams = circle.getLayoutParams();
-
         circleParams.height =  (int)Math.round(height*.25);
         circleParams.width = (int)Math.round(height*.25);
+
+
+        //Text Formatting inside the EventImage Bubble
+
+        //The EventName being Formatted.
+        TextView EventName = (TextView) convertView.findViewById(R.id.TextEventName);
+        int x1= (int)Math.round(height*.0290);
+        EventName.setTextSize(TypedValue.COMPLEX_UNIT_PX,x1);
+
+        //The EventDate being formatted.
+        TextView EventDate = (TextView) convertView.findViewById(R.id.TextEventDate);
+        int x2= (int)Math.round(height*.0295);
+        EventDate.setTextSize(TypedValue.COMPLEX_UNIT_PX,x2);
+
+        //The EventPrice being formatted.
+        TextView EventPrice = (TextView) convertView.findViewById(R.id.TextEventPrice);
+        int x3= (int)Math.round(height*.0300);
+        EventPrice.setTextSize(TypedValue.COMPLEX_UNIT_PX,x3);
+
+        //The EventDistance being formatted.
+        TextView EventDistance = (TextView) convertView.findViewById(R.id.TextEventDistance);
+        int x4= (int)Math.round(height*.020);
+        EventDistance.setTextSize(TypedValue.COMPLEX_UNIT_PX,x4);
+
+
+        //TextViews inside the Detailed view being formatted.
+
+        //The DesEventName being Formatted.
+        TextView DesEventName = (TextView) convertView.findViewById(R.id.DesEventName);
+        int y= (int)Math.round(height*.028);
+        DesEventName.setTextSize(TypedValue.COMPLEX_UNIT_PX,y);
+
+        //The DesEventPrice being formatted.
+        TextView DesPrice = (TextView) convertView.findViewById(R.id.DesEventPrice);
+        int y12= (int)Math.round(height*.028);
+        DesPrice.setTextSize(TypedValue.COMPLEX_UNIT_PX,y12);
+
+        //The DesLocName being Formatted.
+        TextView DesLocName = (TextView) convertView.findViewById(R.id.DesEventLocName);
+        int y1= (int)Math.round(height*.018);
+        DesLocName.setTextSize(TypedValue.COMPLEX_UNIT_PX,y1);
+
+        //The DesLocSt being formatted.
+        TextView DesLocSt = (TextView) convertView.findViewById(R.id.DesEventLocStreet);
+        int y2= (int)Math.round(height*.018);
+        DesLocSt.setTextSize(TypedValue.COMPLEX_UNIT_PX,y2);
+
+        //The DesLocAdd being Formatted.
+        TextView DesLocAdd = (TextView) convertView.findViewById(R.id.DesEventLocAddress);
+        int y3= (int)Math.round(height*.018);
+        DesLocAdd.setTextSize(TypedValue.COMPLEX_UNIT_PX,y3);
+
+        //The DesStartDate being formatted.
+        TextView DesStartDate = (TextView) convertView.findViewById(R.id.DesEventStartDate);
+        int y4= (int)Math.round(height*.014);
+        DesStartDate.setTextSize(TypedValue.COMPLEX_UNIT_PX, y4);
+
+        //The DesStartTime being formatted.
+        TextView DesStartTime = (TextView) convertView.findViewById(R.id.DesEventStartTime);
+        int y5= (int)Math.round(height*.014);
+        DesStartTime.setTextSize(TypedValue.COMPLEX_UNIT_PX,y5);
+
+        //The DesEndDate being formatted.
+        TextView DesEndDate = (TextView) convertView.findViewById(R.id.DesEventEndDate);
+        int y6= (int)Math.round(height * .014);
+        DesEndDate.setTextSize(TypedValue.COMPLEX_UNIT_PX,y6);
+
+        //The DesEndTime being formattted.
+        TextView DesEndTime = (TextView) convertView.findViewById(R.id.DesEventEndTime);
+        int y7= (int)Math.round(height*.014);
+        DesEndTime.setTextSize(TypedValue.COMPLEX_UNIT_PX,y7);
+
+        //The TextView saying "start" being formatted.
+        TextView start = (TextView) convertView.findViewById(R.id.StartText);
+        int y8= (int)Math.round(height*.0127);
+        start.setTextSize(TypedValue.COMPLEX_UNIT_PX,y8);
+
+        //The TextView saying "end" being formatted.
+        TextView end = (TextView) convertView.findViewById(R.id.EndText);
+        int y9= (int)Math.round(height*.0127);
+        end.setTextSize(TypedValue.COMPLEX_UNIT_PX,y9);
+
+
+        //Swipe Bars being resized.
+
+        //The TextView "LOLi" that helps set size of right swipe bar being formatted.
+        TextView Swipe1 = (TextView) convertView.findViewById(R.id.SwipeBarsize1);
+        int yx9= (int)Math.round(height*.030);
+        Swipe1.setTextSize(TypedValue.COMPLEX_UNIT_PX,yx9);
+
+        //The TextView "LOLi" that helps set size of right swipe bar being formatted.
+        TextView Swipe2 = (TextView) convertView.findViewById(R.id.SwipeBarSize2);
+        int yx8= (int)Math.round(height*.030);
+        Swipe2.setTextSize(TypedValue.COMPLEX_UNIT_PX,yx8);
+
+        //The TextView "LOLi" that helps set size of right swipe bar being formatted.
+        TextView Swipe3 = (TextView) convertView.findViewById(R.id.SwipeBarSize3);
+        int yx7= (int)Math.round(height*.030);
+        Swipe3.setTextSize(TypedValue.COMPLEX_UNIT_PX,yx7);
+
     }
 
     //A view holder that contain the things that need to be changed for every event
