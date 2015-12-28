b8b14f1c0ef0f0a8ce4ef81f0402ea33392ee72f
GithubBell <ishtiaba@mcmaster.ca>
2015-12-28 07:33:31 -0500

    First 2 tasks completed, just gotta do it for org venue but for events its done the swipe bars left to right

--- app/src/main/java/com/bruha/bruha/Views/ShowOnMapActivity.java
+++ app/src/main/java/com/bruha/bruha/Views/ShowOnMapActivity.java
@@ -3,6 +3,10 @@
 package com.bruha.bruha.Views;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import com.bruha.bruha.Model.Artist;
@@ -12,9 +16,13 @@ import com.bruha.bruha.Model.SQLiteDatabaseModel;
 import com.bruha.bruha.Model.Venue;
 import com.bruha.bruha.Processing.SQLiteUtils;
 import com.bruha.bruha.R;
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -137,6 +145,10 @@ public class ShowOnMapActivity extends FragmentActivity {
             LatLng eventLocation = new LatLng(event.getEventLatitude(), event.getEventLongitude());
 
             Marker eventMarker = mMap.addMarker(new MarkerOptions().position(eventLocation).title(event.getEventName()));
+
+            Bitmap bitmapIcon = setEventIcon(event);
+            BitmapDescriptor bitmapDescriptorIcon = BitmapDescriptorFactory.fromBitmap(bitmapIcon);
+            eventMarker.setIcon(bitmapDescriptorIcon);
         }
 
        else if(type.equals("Outfit"))
@@ -145,6 +157,9 @@ public class ShowOnMapActivity extends FragmentActivity {
             LatLng eventLocation = new LatLng(outfit.getLat(), outfit.getLng());
 
             Marker eventMarker = mMap.addMarker(new MarkerOptions().position(eventLocation).title(outfit.getOrgName()));
+            Bitmap bitmapIcon = setOrgIcon(outfit);
+            BitmapDescriptor bitmapDescriptorIcon = BitmapDescriptorFactory.fromBitmap(bitmapIcon);
+            eventMarker.setIcon(bitmapDescriptorIcon);
         }
 
        else if(type.equals("Venue"))
@@ -153,6 +168,9 @@ public class ShowOnMapActivity extends FragmentActivity {
             LatLng eventLocation = new LatLng(venue.getLat(), venue.getLng());
 
             Marker eventMarker = mMap.addMarker(new MarkerOptions().position(eventLocation).title(venue.getVenueName()));
+            Bitmap bitmapIcon = setVenueIcon(venue);
+            BitmapDescriptor bitmapDescriptorIcon = BitmapDescriptorFactory.fromBitmap(bitmapIcon);
+            eventMarker.setIcon(bitmapDescriptorIcon);
 
         }
 
@@ -167,8 +185,7 @@ public class ShowOnMapActivity extends FragmentActivity {
 
     }
 
-private void init()
-{   //The method that calls the local database and sets all the variables accordingly.
+    private void init() {   //The method that calls the local database and sets all the variables accordingly.
     // Create the local DB object
     SQLiteDatabaseModel dbHelper = new SQLiteDatabaseModel(this);
     SQLiteUtils sqLiteUtils = new SQLiteUtils();
@@ -179,4 +196,199 @@ private void init()
     mOutfit= sqLiteUtils.getOrganizationsInfo(dbHelper);
     mArtists= sqLiteUtils.getArtistInfo(dbHelper);
 }
+
+    //Method to set the icon of the event.
+    public Bitmap setEventIcon(Event event) {
+        if(event.getEventPrimaryCategory().contains("Club"))
+        {return svgToBitmap(getResources(), R.raw.club, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Performing"))
+        {return svgToBitmap(getResources(), R.raw.performing, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Business"))
+        {return svgToBitmap(getResources(), R.raw.business, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Ceremony"))
+        {return svgToBitmap(getResources(), R.raw.ceremony, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Tech"))
+        {return svgToBitmap(getResources(), R.raw.tech, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Comedy"))
+        {return svgToBitmap(getResources(), R.raw.comedy, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Fashion"))
+        {return svgToBitmap(getResources(), R.raw.fashion, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Festivals"))
+        {return svgToBitmap(getResources(), R.raw.festivals, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Film"))
+        {return svgToBitmap(getResources(), R.raw.film, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Food"))
+        {return svgToBitmap(getResources(), R.raw.food, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Party"))
+        {return svgToBitmap(getResources(), R.raw.party, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Music"))
+        {return svgToBitmap(getResources(), R.raw.music, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Political"))
+        {return svgToBitmap(getResources(), R.raw.political, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("School"))
+        {return svgToBitmap(getResources(), R.raw.school, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Sports"))
+        {return svgToBitmap(getResources(), R.raw.sports, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Tour"))
+        {return svgToBitmap(getResources(), R.raw.tour, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Arts"))
+        {return svgToBitmap(getResources(), R.raw.arts, 30);}
+
+        else if(event.getEventPrimaryCategory().contains("Social"))
+        {return svgToBitmap(getResources(), R.raw.social, 30);}
+
+        return null;
+    }
+
+    //Method to set the icon of the venue
+    public Bitmap setVenueIcon(Venue venue) {
+        if(venue.getVenuePrimaryCategory().contains("Amphitheatre"))
+        {return svgToBitmap(getResources(), R.raw.venamphiteather, 30);}
+
+        if(venue.getVenuePrimaryCategory().contains("Arena"))
+        {return svgToBitmap(getResources(), R.raw.venarena, 30);}
+
+        else if(venue.getVenuePrimaryCategory().contains("Bar/Pub"))
+        {return svgToBitmap(getResources(), R.raw.venbars, 30);}
+
+        else if(venue.getVenuePrimaryCategory().contains("Casino"))
+        {return svgToBitmap(getResources(), R.raw.vencasino, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Church"))
+        {return svgToBitmap(getResources(), R.raw.venchurch, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Cinema"))
+        {return svgToBitmap(getResources(), R.raw.vencinema, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Club"))
+        {return svgToBitmap(getResources(), R.raw.venclubs, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Coffee"))
+        {return svgToBitmap(getResources(), R.raw.vencoffee, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Comedy"))
+        {return svgToBitmap(getResources(), R.raw.vencomedy, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Community"))
+        {return svgToBitmap(getResources(), R.raw.vencommunity, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Fairgrounds"))
+        {return svgToBitmap(getResources(), R.raw.venfairground, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Gallery"))
+        {return svgToBitmap(getResources(), R.raw.vengallery, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Park"))
+        {return svgToBitmap(getResources(), R.raw.venparks, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Restaurant"))
+        {return svgToBitmap(getResources(), R.raw.venrestauratns, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("House/Residence"))
+        {return svgToBitmap(getResources(), R.raw.venhouse, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("School"))
+        {return svgToBitmap(getResources(), R.raw.venschool, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Sports/Arena"))
+        {return svgToBitmap(getResources(), R.raw.venarena, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Store"))
+        {return svgToBitmap(getResources(), R.raw.venstore, 30);}
+
+        else if (venue.getVenuePrimaryCategory().contains("Theatre"))
+        {return svgToBitmap(getResources(), R.raw.ventheater, 30);}
+
+        return svgToBitmap(getResources(), R.raw.vencommunity, 30);
+    }
+
+    //Method to set the icon of the event.
+    public Bitmap setOrgIcon(Organizations org) {
+        if(org.getOrgPrimaryCategory().contains("Academic"))
+        {return svgToBitmap(getResources(), R.raw.orgacademic, 30);}
+
+        else if(org.getOrgPrimaryCategory().contains("Business"))
+        {return svgToBitmap(getResources(), R.raw.orgbusiness, 30);}
+
+        else if(org.getOrgPrimaryCategory().contains("Charity"))
+        {return svgToBitmap(getResources(), R.raw.orgcharity, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Fashion"))
+        {return svgToBitmap(getResources(), R.raw.orgfashion, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Promoter"))
+        {return svgToBitmap(getResources(), R.raw.orgpromoter, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Fraternity"))
+        {return svgToBitmap(getResources(), R.raw.orgfraternity, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Not-for-profit"))
+        {return svgToBitmap(getResources(), R.raw.orgnonprofit, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Sports"))
+        {return svgToBitmap(getResources(), R.raw.orgsports, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Student"))
+        {return svgToBitmap(getResources(), R.raw.orgstudent, 30);}
+
+        else if (org.getOrgPrimaryCategory().contains("Religion"))
+        {return svgToBitmap(getResources(), R.raw.orgreligon, 30);}
+
+        return svgToBitmap(getResources(), R.raw.orgpromoter, 30);
+    }
+
+    //SVG Conversion:
+    public BitmapDrawable svgToBitmapDrawable(Resources res, int resource, int size){
+        try {
+            size = (int)(size*res.getDisplayMetrics().density);
+            SVG svg = SVG.getFromResource(getApplicationContext(), resource);
+
+            Bitmap bmp = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bmp);
+            svg.renderToCanvas(canvas);
+
+            BitmapDrawable drawable = new BitmapDrawable(res, bmp);
+
+
+            return drawable;
+        }catch(SVGParseException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Bitmap svgToBitmap(Resources res, int resource, int size) {
+        try {
+            size = (int)(size*res.getDisplayMetrics().density);
+            SVG svg = SVG.getFromResource(getApplicationContext(), resource);
+
+            Bitmap bmp;
+            bmp = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bmp);
+            svg.renderToCanvas(canvas);
+
+
+            return bmp;
+        } catch (SVGParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
