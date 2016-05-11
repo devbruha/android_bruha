c9798e1c50ad9898d4796e79134023cf5a14afa8
Shaad <lakdawsm@mcmaster.ca>
2016-05-11 17:18:28 -0400

    Started Implementation of JAVA code of the new ProfileActivity

--- app/src/main/java/com/bruha/bruhaandroid/Views/ProfileActivity.java
+++ app/src/main/java/com/bruha/bruhaandroid/Views/ProfileActivity.java
@@ -2,14 +2,35 @@ package com.bruha.bruhaandroid.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bruha.bruhaandroid.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    @InjectView(R.id.profilePageTitle) TextView mPageTitle;
+    @InjectView(R.id.profilePageAddictionText) TextView mPageAddictionText;
+    @InjectView(R.id.profilePageAddictionPic) ImageView mPageAddictionImage;
+    @InjectView(R.id.profilePageUploadText) TextView mPageUploadText;
+    @InjectView(R.id.profilePageUploadPic) ImageView mPageUploadImage;
+    @InjectView(R.id.profilePageUserName) TextView mPageUserName;
+    @InjectView(R.id.profilePageUserEmail) TextView mPageUserEmail;
+    @InjectView(R.id.profilePageUserPic) ImageView mPageUserPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.inject(this);
     }
+
+
+
+
+
+
 }
