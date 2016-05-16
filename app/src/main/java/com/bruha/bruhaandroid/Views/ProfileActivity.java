f7404a8127fb327a7d67a2ae2bff54ce731493ad
ArhamStuff <arhumraza95@gmail.com>
2016-05-16 11:07:20 -0400

    Pushing everything before working on fragments

--- app/src/main/java/com/bruha/bruhaandroid/Views/ProfileActivity.java
+++ app/src/main/java/com/bruha/bruhaandroid/Views/ProfileActivity.java
@@ -12,7 +12,7 @@ import butterknife.InjectView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    @InjectView(R.id.profilePageTitle) TextView mPageTitle;
+   // @InjectView(R.id.profilePageTitle) TextView mPageTitle;
     @InjectView(R.id.profilePageAddictionText) TextView mPageAddictionText;
     @InjectView(R.id.profilePageAddictionPic) ImageView mPageAddictionImage;
     @InjectView(R.id.profilePageUploadText) TextView mPageUploadText;
